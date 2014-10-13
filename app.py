dc6c55c712bc87199085da929bcdc6a2a92b5218
Roel <nyubis@gmail.com>
2014-10-13 19:05:30 +0300

    latin-1 to utf-8 now works for both py2 and py3.

--- app.py
+++ app.py
@@ -24,17 +24,34 @@ def lookupquote(nick=None, encoding=None):
 	if len(rows) == 0:
 		resp = "No quotes found" if encoding == None else """{"error": "No quotes found for this nick"}"""
 		return Response(response=resp, mimetype=mimetype)
+
+	decode = decode_py2 if check_py_version() == 2 else decode_py3
+
 	if encoding == None:
 		results = ""
 		for row in rows:
-			results += "<%s> %s\n" % (nick, row[0].decode('utf-8'))
+			results += "<%s> %s\n" % (nick, decode(row[0]))
+
 		resp = Response(response=results, mimetype=mimetype)
 		return resp
 	elif encoding == "json":
 		results = {"nick": nick, "quotes": []}
 		for row in rows:
-			results["quotes"].append(row[0].decode('utf-8'))
+			results["quotes"].append(decode(row[0]))
 		return Response(response=json.dumps(results), mimetype=mimetype)
+
+def check_py_version():
+	""" Returns the major python version currently used """
+	import sys
+	return sys.version_info[0]
+
+def decode_py3(data):
+	""" Decode a latin-1 string and turn it into utf-8, the Python 3 way """
+	return bytes(data, 'latin-1').decode('utf-8')
+
+def decode_py2(data):
+	""" Decode a latin-1 string and turn it into utf-8, the Python 3 way """
+	return data.decode('utf-8')
 
 if __name__ == "__main__":
 	app.run(debug=True)
