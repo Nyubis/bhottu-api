3ae42553acfd81b67281364cbb56cd4dd01d34cf
Roel <nyubis@gmail.com>
2015-03-17 11:27:49 -0400

    Added searching support, dropped python 2 support
    Python 3 is the future!

--- app.py
+++ app.py
@@ -1,41 +1,49 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 from flask import Flask, Response
 import json
 from config import *
-try:
-	import MySQLdb #python 2
-except ImportError:
-	import pymysql as MySQLdb #python 3 substitute
+import pymysql as MySQLdb 
 
 app = Flask(__name__)
+
+@app.route("/search/<keyword>/")
+@app.route("/search/<keyword>/<encoding>")
+def search(keyword=None, encoding=None):
+	if keyword == None:
+		return "Pass a keyword"
+
+	db = MySQLdb.connect(host=DB_HOST, user=DB_USER, passwd=DB_PASSWORD, db=DB_SCHEME)
+	cursor = db.cursor()
+	cursor.execute("SELECT name, quotation FROM quote WHERE quotation LIKE %s", ("%"+keyword+"%"))
+	rows = cursor.fetchall()
+
+	return format(rows, encoding)
 
 @app.route("/quotes/<nick>/")
 @app.route("/quotes/<nick>/<encoding>")
 def lookupquote(nick=None, encoding=None):
-	mimetype = "text/plain" if encoding == None else "application/json"
 	if nick == None:
-		resp = "Pass a nick" if encoding == None else """{"error": "No nick provided"}"""
-		return Response(response=resp, mimetype=mimetype)
+		return "Pass a nick"
 	db = MySQLdb.connect(host=DB_HOST, user=DB_USER, passwd=DB_PASSWORD, db=DB_SCHEME)
 	cursor = db.cursor()
 
-	cursor.execute("SELECT quotation FROM quote WHERE name = %s", [nick])
+	cursor.execute("SELECT name, quotation FROM quote WHERE name = %s", [nick])
 	rows = cursor.fetchall()
-	if len(rows) == 0:
-		resp = "No quotes found" if encoding == None else """{"error": "No quotes found for this nick"}"""
-		return Response(response=resp, mimetype=mimetype)
 
-	#Transforming the data from the db into utf-8 is version-dependent
-	decode = decode_py2 if check_py_version() == 2 else decode_py3
+	return format(rows, encoding)
+
+def format(rows, encoding):
+	mimetype = "text/plain" if encoding == None else "application/json"
+	if len(rows) == 0:
+		resp = "No quotes found" if encoding == None else """{"error": "No quotes found"}"""
+		return Response(response=resp, mimetype=mimetype)
 
 	if encoding == None:
 		#Return it as plaintext
 		results = ""
-		for row in rows:
-			results += "<%s> %s\n" % (nick, decode(row[0]))
+		results = "\n".join(map(lambda x: "<%s> %s" % (x[0], decode(x[1])), rows))
+		return Response(response=results, mimetype=mimetype)
 
-		resp = Response(response=results, mimetype=mimetype)
-		return resp
 	elif encoding == "json":
 		#Return it as json
 		results = {"nick": nick, "quotes": []}
@@ -43,20 +51,11 @@ def lookupquote(nick=None, encoding=None):
 			results["quotes"].append(decode(row[0]))
 		return Response(response=json.dumps(results), mimetype=mimetype)
 
-def check_py_version():
-	""" Returns the major python version currently used """
-	import sys
-	return sys.version_info[0]
-
-def decode_py3(data):
-	""" Decode a latin-1 string and turn it into utf-8, the Python 3 way """
-	return bytes(data, 'latin-1').decode('utf-8')
-
-def decode_py2(data):
-	""" Decode a latin-1 string and turn it into utf-8, the Python 2 way """
-	return data.decode('utf-8')
+def decode(data):
+	""" Decode a latin-1 string and turn it into utf-8 """
+	return data.encode("latin-1").decode("utf-8", errors="replace")
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(host="0.0.0.0", port=8000)
 else:
 	application = app
